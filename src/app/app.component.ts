fcc087423a0e1bd1f4ba249b6cb6457bf262b423
Harris Weaver <harrisweaver@gmail.com>
2018-09-22 00:25:34 -0400

    Step 0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'stepsintovoid';
+  steps: any = {
+    0: false,
+    1: false
+  };
+
+  activateStep = function(stepNumber) {
+    this.steps[stepNumber] = true;
+  };
+
 }
